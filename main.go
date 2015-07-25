40123022754aa01540e753ac6278c713d81c73cb
Daniel TISCHER <dos.7182@gmail.com>
2015-07-25 19:57:17 +0200

    added build scripts

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	"os"
 )
 
-const version string = "1.0.0"
+// http://technosophos.com/2014/06/11/compile-time-string-in-go.html
+// go build -ldflags "-x main.version $(git describe --tags)"
+var version string
 
 func main() {
 	hkcu := flag.String("hkcu", "REQUIRED", "extract HKEY_CURRENT_USER environment to file")
